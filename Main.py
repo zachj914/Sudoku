9eb457aa224cc4591ad428979b87059dae1b3df8
Zach Desktop <zach@ucsc.edu>
2020-03-20 13:37:10 -0400

    Removed and replaced outdate Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,9 @@ import pygame
 import Sudoku_Solver
 pygame.init()
 pygame.key.set_repeat(120)
+width, height = 1080, 773
+display = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Sudoku')
 
 
 class Grid(Sudoku_Solver.Board):
@@ -9,9 +12,9 @@ class Grid(Sudoku_Solver.Board):
         '''Loads an image of the board and a rectangle representing it'''
         Sudoku_Solver.Board.__init__(self, values)
         self.board = pygame.image.load('Board.png').convert_alpha()
-        self.board_rect = pygame.Rect(screen.width /
+        self.board_rect = pygame.Rect(width /
                                       2 - self.board.get_width() / 2,
-                                      screen.height /
+                                      height /
                                       2 - self.board.get_height() / 2,
                                       self.board.get_width(),
                                       self.board.get_height())
@@ -38,7 +41,7 @@ class Grid(Sudoku_Solver.Board):
 
     def draw_board(self):
         '''Draws the board, numbers, and cursor onto the screen'''
-        screen.display.blit(self.board, (self.board_rect.left, self.board_rect.top))
+        display.blit(self.board, (self.board_rect.left, self.board_rect.top))
         for row_count, row in enumerate(self.values):
             for num_count, num in enumerate(row):
                 if num != 0:
@@ -57,7 +60,6 @@ class Grid(Sudoku_Solver.Board):
         if self.active_square is not None:
             self.active_cursor = pygame.image.load('Active_Cursor.png').convert_alpha()
             self.draw_square(self.active_cursor, self.active_square)
-        pygame.display.flip()
 
     def get_square(self, mouse_pos):
         if not self.board_rect.collidepoint(mouse_pos):
@@ -79,7 +81,7 @@ class Grid(Sudoku_Solver.Board):
 
     def draw_square(self, image, cords):
         cords = self.get_square_cords(cords)
-        screen.display.blit(image, (cords[0], cords[1]))
+        display.blit(image, (cords[0], cords[1]))
 
     def play_square(self, number):
         row, column = (self.active_square[1], self.active_square[0])
@@ -116,50 +118,17 @@ class Grid(Sudoku_Solver.Board):
         return True if len(self.errors) != 0 else False
 
 
-class Screens:
-    def __init__(self):
-        self.width, self.height = 1080, 773
-        self.display = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption('Sudoku')
-        self.title = pygame.image.load('Titlescreen.jpg').convert()
-
-    def title_display(self):
-        self.play_button = Button(pygame.image.load('PlayText.png').convert_alpha(),
-                                  horz_alignment=1/2, vert_alignment=7/8)
-        self.options_button = Button(pygame.image.load('Options.png').convert_alpha(),
-                                     horz_alignment=1/5, vert_alignment=7/8)
-        self.leaderboard_button = Button(pygame.image.load('Leaderboard.png').convert_alpha(),
-                                         horz_alignment=4/5, vert_alignment=7/8)
-        self.active_buttons = {self.play_button:False, self.options_button:False,
-                               self.leaderboard_button: False}
-        self.display.blit(self.title, (0, 0))
-        for button in self.active_buttons.keys():
-            if self.active_buttons[button] is True:
-                button.draw_active()
-            else:
-                button.draw()
-        pygame.display.flip()
-
-    def play_display(self):
-        self.play = pygame.image.load('Background.jpg').convert()
-        self.display.blit(self.play, (0, 0))
-        board.draw_board()
-
-    def win_display(self):
-        self.win = pygame.image.load('WinScreen.jpg').convert()
-        self.display.blit(self.win, (0, 0))
-        pygame.display.flip()
-
-
 class Button:
     def __init__(self, image, left_corner=(None, None), horz_alignment=None,
                  vert_alignment=None):
         self.image = image
         self.left_corner = list(left_corner)
         if self.left_corner[0] is None:
-            self.left_corner[0] = int(screen.width*horz_alignment) - self.image.get_width()/2
+            self.left_corner[0] = int(width*horz_alignment)\
+                - self.image.get_width()/2
         if self.left_corner[1] is None:
-            self.left_corner[1] = int(screen.height*vert_alignment) - self.image.get_height()/2
+            self.left_corner[1] = int(height*vert_alignment)\
+                - self.image.get_height()/2
         self.rect = pygame.Rect(self.left_corner[0], self.left_corner[1],
                                 self.image.get_width(),
                                 self.image.get_height())
@@ -172,32 +141,59 @@ class Button:
                                     int(self.rect.height * .1))
 
     def draw(self):
-        screen.display.blit(self.image, self.rect)
+        display.blit(self.image, self.rect)
 
     def draw_active(self):
-        screen.display.blit(self.active_image, self.active_rect)
+        display.blit(self.active_image, self.active_rect)
 
 
-# Gameloop
+class Screens:
+    def __init__(self, background, buttons):
+        self.background = pygame.image.load(background).convert()
+        self.buttons = buttons
+
+    def draw(self, active_buttons):
+        display.blit(self.background, (0, 0))
+        for button in self.buttons:
+            if button in active_buttons:
+                button.draw_active()
+            else:
+                button.draw()
+
+
 def title_loop():
     running = True
-    screen.title_display()
+    play_button = Button(pygame.image.load('PlayText.png').convert_alpha(),
+                         horz_alignment=1/2, vert_alignment=7/8)
+    options_button = Button(pygame.image.load('Options.png').convert_alpha(),
+                            horz_alignment=1/5, vert_alignment=7/8)
+    leaderboard_button = Button(pygame.image.load(
+                                'Leaderboard.png').convert_alpha(),
+                                horz_alignment=4/5, vert_alignment=7/8)
+    screen = Screens('Titlescreen.jpg', {play_button, options_button,
+                                         leaderboard_button})
+    active_buttons = set()
+    screen.draw(active_buttons)
+    pygame.display.flip()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse = pygame.mouse.get_pos()
-                if screen.play_button.rect.collidepoint(mouse):
+                if play_button.rect.collidepoint(mouse):
                     play_loop()
                     running = False
 
         mouse = pygame.mouse.get_pos()
-        prev_buttons = tuple(screen.active_buttons.values())
-        for button in screen.active_buttons.keys():
-            screen.active_buttons[button] = True if button.rect.collidepoint(mouse) else False
-        if tuple(screen.active_buttons.values()) != prev_buttons:
-            screen.title_display()
+        prev_buttons = set(active_buttons)
+        active_buttons = set()
+        for button in screen.buttons:
+            if button.rect.collidepoint(mouse):
+                active_buttons.add(button)
+        if active_buttons != prev_buttons:
+            screen.draw(active_buttons)
+            pygame.display.flip()
     pygame.time.Clock().tick(30)
 
 
@@ -206,10 +202,12 @@ def play_loop():
                 pygame.K_4: 4, pygame.K_5: 5, pygame.K_6: 6, pygame.K_7: 7,
                 pygame.K_8: 8, pygame.K_9: 9}
     arrow_keys = {pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT}
-    screen.play_display()
+    screen = Screens('Background.jpg', set())
     running = True
     while running:
-        screen.play_display()
+        screen.draw(set())
+        board.draw_board()
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -232,21 +230,23 @@ def play_loop():
                             board.active_square[0] -= 1
                         if key == pygame.K_RIGHT:
                             board.active_square[0] += 1
-                if not board.board_rect.collidepoint(board.get_square_cords(board.active_square)):
+                if not board.board_rect.collidepoint(board.get_square_cords(
+                                                     board.active_square)):
                     board.active_square = current_square
         pygame.time.Clock().tick(30)
 
 
 def win_loop():
-    screen.win_display()
+    screen = Screens('WinScreen.jpg', set())
     running = True
+    screen.draw(set())
+    pygame.display.flip()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
 
-screen = Screens()
 board = Grid([[6, 7, 0, 3, 1, 0, 0, 9, 0],
               [0, 0, 3, 0, 0, 4, 2, 0, 5],
               [4, 0, 0, 0, 5, 0, 0, 7, 0],
